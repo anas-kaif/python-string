3f6f18fd8892cde49cd58ef8887cf8b3a4e69777
anas kaif <mdkaif3120@gmail.com>
2026-02-20 14:58:04 +0530

    OOPs first program

--- OOPs/program1.py
+++ OOPs/program1.py
@@ -1,10 +1,12 @@
 class Student:
-    name="Anas"
-    age=21
-    def __init__(self):
-        print("hello")
-    def __init__(self,name):
+    def __init__(self,name,age):
         self.name=name
+        self.age=age
 
-s1=Student()
-print(s1.name)
+    def show(self):
+        print(self.name,self.age)
+
+s1=Student("Anas",22)
+s1.name="kaif"
+s1.age=20
+s1.show()
