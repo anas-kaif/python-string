bb1cae257d68bba2cac362bd04e1055a987e5471
anas kaif <mdkaif3120@gmail.com>
2026-02-23 08:52:31 +0530

    Shopping Cart System with full control options

--- OOPs/Encapsulation/problem6_Shopping_Cart_System.py
+++ OOPs/Encapsulation/problem6_Shopping_Cart_System.py
@@ -16,11 +16,29 @@ class Cart:
         return total
 sp1=Cart()
 
-sp1.Add_item("chips",10)
-sp1.Add_item("cake",20)
-sp1.Show_items()
-print(sp1.Show_total())
-print("After Remove item")
-sp1.Remove_items("chips")
-sp1.Show_items()
-print(sp1.Show_total())
+print("Welcome to Cart System!\n")
+print("(1)Add items      (2)Remove item\n(3)Show items      (4)Show total\n(5)Exit")
+choice=0
+while choice != 5:
+    choice=int(input("Enter your option:"))
+
+    if choice==1:
+        name=input("Enter item name:")
+        price=int(input("Enter item price:"))
+        sp1.Add_item(name,price)
+
+    elif choice==2:
+        name=input("Enter item to remove:")
+        sp1.Remove_items(name)
+
+    elif choice==3:
+        sp1.Show_items()
+
+    elif choice==4:
+        print("Total:",sp1.Show_total())
+
+    elif choice==5:
+        print("Thank you!")
+
+    else:
+        print("Invalid option")
