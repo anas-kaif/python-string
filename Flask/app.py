a7ea62ce1fa4ae3db379ca3bab8e80cc265042a4
anas kaif <mdkaif3120@gmail.com>
2026-03-06 08:07:27 +0530

    Student management system with  Home,Course,Admin pages

--- Flask/app.py
+++ Flask/app.py
@@ -1,5 +1,27 @@
-from flask import Flask 
+from flask import Flask,render_template
 app=Flask(__name__)
-@app.route("/<name>")
-def home(name):
-    return f"name is {name}"
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/courses")
+def course():
+    return render_template("course.html")
+@app.route("/admin")
+def admin():
+    users = [
+        {"id": 1, "name": "Md", "role": "Admin"},
+        {"id": 2, "name": "Ali", "role": "Teacher"},
+        {"id": 3, "name": "Sara", "role": "Student"},
+    ]
+    courses = [
+        {"id": "C101", "name": "Python Programming"},
+        {"id": "C102", "name": "Web Development"},
+        {"id": "C103", "name": "Database Management"},
+    ]
+    reports = [
+        {"title": "Student Performance Report"},
+        {"title": "Course Enrollment Report"},
+        {"title": "System Usage Report"},
+    ]
+    return render_template("admin.html",users=users,courses=courses,reports=reports)
